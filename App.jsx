9490bace93d4772f679fcbd4ccc9f23a3e7b08ac
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-20 14:54:59 -0700

    Added header section

--- App.jsx
+++ App.jsx
@@ -1,33 +1,10 @@
-import React from "react"
-
-/**
- * Project planning:
- * 
- * 
- * - What are the main containers of elements I need
- *   in this app?
- *  - Header
- *  - Message container for status of game
- *  - Container for letter slots
- *  - Letter keypads
- *  - New game button
- * - What values will need to be saved in state vs.
- *   what values can be derived from the state?
- * - The letter keypad array has to have a state
- * - The actual letters of the word has to also have a state
- * - The number of wrong guesses should be stored in a state
- * - The game status can be derived for the state of the actual letters of word
- * - How will the user interact with the app? What
- *   events do I need to handle?
- * The user will click on the letter keypads to reveal the letters on the word
- * and press on new game to start a new game
- * User can also use the keyboard to toggle between the letter keypads
- */
+import React from 'react';
+import Header from './components/Header';
 
 export default function Hangman() {
     return (
         <main>
-            Game goes here
+            <Header />
         </main>
-    )
+    );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h1 className="title">Assembly: Endgame</h1>
+            <p className="instructions">
+                Guess the word in under 8 attempts to keep the programming world
+                safe from Assembly!
+            </p>
+        </header>
+    );
+}
